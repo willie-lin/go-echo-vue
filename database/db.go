5849815f8c26880b4c8fd8409686f369f4a0a4b2
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2020-04-26 15:22:11 +0800

    add db

---
+++ database/db.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+func Connect() *gorm.DB{
+	db, err := gorm.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/todo?charset=utf8&parseTime=true")
+	//
+
+	if err != nil {
+		fmt.Print("データベース接続に失敗しました。")
+	}
+	db.LogMode(true)
+	return db
+}
